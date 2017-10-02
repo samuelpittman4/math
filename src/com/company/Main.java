19ad7bc6ff728a9dbb787f629b706b04ab30da7c
Samuel Pittman <samuelpittman@Samuels-MacBook-Air.local>
2017-10-02 19:21:21 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main( String[] args ) {
+
+        int mynumber;
+
+        double myothernumber;
+
+
+        mynumber = 2;
+
+        myothernumber = 1.7938;
+
+
+
+        System.out.println(mynumber );
+        System.out.println(myothernumber);
+
+
+
+
+    }
+
+}
